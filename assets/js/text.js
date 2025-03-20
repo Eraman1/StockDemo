4b54154356dacdf8e103243b90822cf9723d99c0
Eraman1 <er.aman614@gmail.com>
2025-03-20 15:48:18 +0530

    add bill share page

---
+++ assets/js/text.js
@@ -0,0 +1,12 @@
+const profile ={
+     userName: "Aman614",
+     isFollow: true,
+     followers:123,
+     followings:234,
+     posts:23,
+    FullName:"Aman Adee",
+};
+
+const selectedColor = ['aman', '28', 34];
+
+console.log(selectedColor[1]);
